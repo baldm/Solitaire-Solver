d5ef94cb40e30ae043a32a73fa21afece271bfd7
Jens <jenswilliversen@gmail.com>
2022-03-16 10:38:12 +0100

    added action_model and state_model
    Co-Authored-By: Andreas Isaksson <3878+isak@users.noreply.github.com>

---
+++ backend/model/state_model.py
@@ -0,0 +1,11 @@
+class State():
+    def __init__(self,board,foundations):
+        self.board = board
+        self.foundations = foundations
+        self.prev_state = self
+
+    def equals(self,state):
+        if self.board == state.board and self.foundations == state.foundations:
+            return True
+        return False
+
