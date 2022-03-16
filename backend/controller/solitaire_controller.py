0bf5b4bf8f55f293a2d998c1d794ae3df1ba4c62
Jens <jenswilliversen@gmail.com>
2022-03-16 11:37:05 +0100

    added rules for descending order, alternating color and if king moves to empty row
    Co-Authored-By: Andreas Isaksson <3878+isak@users.noreply.github.com>

--- backend/controller/solitaire_controller.py
+++ backend/controller/solitaire_controller.py
@@ -3,6 +3,10 @@ from model.state_model import State_model
 
 class Solitaire_controller():
     def __init__(self):
+        values = range(1,13)
+        keys = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+        self.order = dict(keys,values)
+
         pass
 
     def action(self, state : State_model):
@@ -17,7 +21,7 @@ class Solitaire_controller():
                     continue
                 for to_row in range(0,len(board) + len(foundations) + 1):
                     action = Action_model(card_index, row, to_row)
-                    if self.is_move_legal(action):
+                    if self.is_move_legal(action, state):
                         actions.append(action)
         return actions
         
@@ -26,9 +30,33 @@ class Solitaire_controller():
         ## applies a given action to a given state and returns state
         pass
 
-    def is_move_legal():
+    def is_move_legal(actions : Action_model, state : State_model):
         ## checks if an action is valid
         pass
+
+    def descending_order(self,card : str, card_to : str):
+
+        if self.order.get(card_to[0]) - 1 == self.order.get(card[0]):
+            return True
+        return False
+    
+    def king_to_empty(card : str, to_row : list):
+        if card[0] == 'K':
+            if not to_row:
+                return True
+        return False
+
+    def alternating_color(card : str, card_to : str):
+        if (card[1] == 'S' or card[1] == 'C') and (card_to[1] == 'D' or card_to[1] == 'H') :
+            return True
+        elif (card[1] == 'D' or card[1] == 'H') and (card_to[1] == 'S' or card_to[1] == 'C'):
+            return True
+        return False
+        
+       
+    
+
+
 
     def is_terminal():
         ## returns bool
