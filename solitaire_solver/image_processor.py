3d959f16a266a9cd457fe54eb91191882ab25308
baldm <jonathan@zoern.dk>
2022-04-27 09:32:01 +0200

    Added randomly generated cards to move and an empty file to keep a folder from being deleted

--- solitaire_solver/image_processor.py
+++ solitaire_solver/image_processor.py
@@ -1,5 +1,6 @@
 from .card_recognizer import CardRecognizer
 
+import random
 
 def process_and_analyze_image(image):
 
@@ -11,4 +12,7 @@ def process_and_analyze_image(image):
     # main analyzing things go here
 
     # Change to analyzers output, e.i the next move
-    return {'move_from': 2, 'move_card': 'temp card', 'move_to': 3}
+    move_from = random.randint(0, 7)
+    move_to = random.randint(0, 7)
+    move_card = random.choice(['1 Clubs','2 Spades','5 Clubs', '8 Hearts'])
+    return {'move_from':move_from, 'move_card': move_card, 'move_to': move_to}
