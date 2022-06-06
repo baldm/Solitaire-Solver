2e07863808e8b37aa76ae61cb9e75eeacdeaa5a5
irisswt <isakrisager@hotmail.com>
2022-06-06 21:40:12 +0200

    created test for test_king_to_empty method

--- tests/solitaire_controller_tests/test_solitaire_controller.py
+++ tests/solitaire_controller_tests/test_solitaire_controller.py
@@ -3,6 +3,7 @@ from solitaire_solver.card_analyzer.controller import solitaire_controller
 
 
 class TestSolitaire_controller(TestCase):
+    ## Tests all combinations of colors being moved
     def test_alternating_color(self):
         card_diamond = "3D"
         card_heart = "4H"
@@ -24,3 +25,15 @@ class TestSolitaire_controller(TestCase):
         self.assertFalse(solitaire_controller.Solitaire_controller.alternating_color(self, card_heart, card_diamond))
         self.assertFalse(solitaire_controller.Solitaire_controller.alternating_color(self, card_diamond, card_diamond))
         self.assertFalse(solitaire_controller.Solitaire_controller.alternating_color(self, card_diamond, card_heart))
+
+    ## Test if king moves to an empty column and populated column
+    def test_king_to_empty(self):
+        card_king = "KH"
+        empty_list = []
+        list = ["2H, 3C"]
+        self.assertTrue(solitaire_controller.Solitaire_controller.king_to_empty(self, card_king, empty_list))
+        self.assertFalse(solitaire_controller.Solitaire_controller.king_to_empty(self, card_king, list))
+
+
+    def test_descending_order(self):
+        self.fail()
