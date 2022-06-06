8ba7ff3d8669d9c1254b246108a2735e793f44ba
irisswt <70897754+irisswt@users.noreply.github.com>
2022-06-06 12:45:10 +0200

    added test for alternating color

--- tests/solitaire_controller_tests/test_solitaire_controller.py
+++ tests/solitaire_controller_tests/test_solitaire_controller.py
@@ -4,6 +4,23 @@ from solitaire_solver.card_analyzer.controller import solitaire_controller
 
 class TestSolitaire_controller(TestCase):
     def test_alternating_color(self):
+        card_diamond = "3D"
         card_heart = "4H"
         card_clubs = "5C"
+        card_spade = "6S"
         self.assertTrue(solitaire_controller.Solitaire_controller.alternating_color(card_heart, card_clubs))
+        self.assertTrue(solitaire_controller.Solitaire_controller.alternating_color(card_heart, card_spade))
+        self.assertTrue(solitaire_controller.Solitaire_controller.alternating_color(card_diamond, card_clubs))
+        self.assertTrue(solitaire_controller.Solitaire_controller.alternating_color(card_diamond, card_spade))
+        self.assertTrue(solitaire_controller.Solitaire_controller.alternating_color(card_clubs, card_heart))
+        self.assertTrue(solitaire_controller.Solitaire_controller.alternating_color(card_clubs, card_diamond))
+        self.assertTrue(solitaire_controller.Solitaire_controller.alternating_color(card_spade, card_heart))
+        self.assertTrue(solitaire_controller.Solitaire_controller.alternating_color(card_spade, card_diamond))
+        self.assertFalse(solitaire_controller.Solitaire_controller.alternating_color(card_clubs, card_clubs))
+        self.assertFalse(solitaire_controller.Solitaire_controller.alternating_color(card_clubs, card_spade))
+        self.assertFalse(solitaire_controller.Solitaire_controller.alternating_color(card_spade, card_spade))
+        self.assertFalse(solitaire_controller.Solitaire_controller.alternating_color(card_spade, card_clubs))
+        self.assertFalse(solitaire_controller.Solitaire_controller.alternating_color(card_heart, card_heart))
+        self.assertFalse(solitaire_controller.Solitaire_controller.alternating_color(card_heart, card_diamond))
+        self.assertFalse(solitaire_controller.Solitaire_controller.alternating_color(card_diamond, card_diamond))
+        self.assertFalse(solitaire_controller.Solitaire_controller.alternating_color(card_diamond, card_heart))
