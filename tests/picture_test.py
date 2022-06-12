deed439c176f7525d6986d67a7ed39417efbbdd4
louismonty <louis.mail.dk@gmail.com>
2022-06-12 19:59:39 +0200

    fix

--- tests/picture_test.py
+++ tests/picture_test.py
@@ -1,6 +1,8 @@
 import os
 import base64
+import cv2
 import requests
+import numpy
 
 paths = os.listdir("cardDetector/billeder")
 
@@ -8,6 +10,19 @@ url = 'http://0.0.0.0:8000'
 timeout = 5
 # Send simple request to check if there is a connection to the backend
 response = requests.get(url, timeout=timeout)
+pictures = []
+merged_picture = None
+#first input
+for i in range(0,6):
+    user_input = input("skriv et kort som foreksempel klør4.jpg (start)")
+    for path in paths:
+        if user_input in path:
+            pictures.append(cv2.imread("cardDetector/billeder/" + path))
+    for picture in pictures:
+        numpy.concatenate((merged_picture,picture),axis=1)
+cv2.imshow("merged",merged_picture)
+cv2.waitKey(0)
+
 
 while True:
     user_input = input("skriv et kort som foreksempel klør4.jpg")
