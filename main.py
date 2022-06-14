8a558a14ac7056b2594b1d1337d5f46dcf041f0d
Nikoditten <48121115+Nikoditten@users.noreply.github.com>
2022-06-14 14:16:29 +0200

    try except - api

--- main.py
+++ main.py
@@ -33,26 +33,29 @@ class Image(BaseModel):
 
 @app.post("/analyze_image")
 async def upload_board_image(item: Image):
-  
-    # Get path where image will be saved
-    save_path = join(getcwd(), 'images', 'filename.png')
-
-    # Decode image from base64
-    decoded_image = base64.b64decode(item.image_string)
-
-    # Save image to drive
-    with open(save_path, 'wb') as file:
-        file.write(decoded_image)
 
     # PROCESS IMAGE HERE:
     # sleep here to demonstrate processing image
     try:
+        # Get path where image will be saved
+        save_path = join(getcwd(), 'images', 'filename.png')
+
+        # Decode image from base64
+        decoded_image = base64.b64decode(item.image_string)
+
+        # Save image to drive
+        with open(save_path, 'wb') as file:
+            file.write(decoded_image)
+
+
         endpoint_output = process_and_analyze_image(save_path)
+        
+        # Removing temp file
+        remove(save_path)
     except Exception as e:
         endpoint_output = [{'move_from': '', 'move_card': '', 'move_to': '', 'get_talon': False}]
         print(e)
 
-    # Removing temp file
-    remove(save_path)
+    
 
     return endpoint_output
