8260df0b985a52499a34666ab0121f8c5f0eb6ce
Nikoditten <48121115+Nikoditten@users.noreply.github.com>
2022-06-14 16:34:34 +0200

    throw exception

--- solitaire_solver/image_processor.py
+++ solitaire_solver/image_processor.py
@@ -17,10 +17,16 @@ def process_and_analyze_image(image):
     print("Card info:")
     print(card_info)
 
-    # Updating board state
-    card_analyzer.update_card(card_info)
-    # main analyzing things go here
-    next_move = card_analyzer.get_next_moves()
+    if len(card_info) != 0:
+        # Updating board state
+        card_analyzer.update_card(card_info)
+        # main analyzing things go here
+        next_move = card_analyzer.get_next_moves()
 
-    print("Next move:")
-    print(next_move)
+        print("Next move:")
+        print(next_move)
+    else:
+        raise Exception("No cards found")
+    
+    return next_move
+    
